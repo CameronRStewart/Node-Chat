dddd3928ed2f0d4c1028d4664d7b9c9810405c00
cameron stewart <bacchus@camerons-iMac.local>
2016-02-02 08:01:29 -0700

    initial commit

---
+++ public/chat.js
@@ -0,0 +1,32 @@
+window.onload = function() {
+	var messages = [];
+	var socket = io.connect('http://localhost:3700');
+	var field = document.getElementById("field");
+	var sendButton = document.getElementById("send");
+	var clearButton = document.getElementById("clear");
+	var content = document.getElementById("content");
+
+	socket.on('message', function (data) {
+		if(data.message) {
+			messages.push(data.message);
+			var html = '';
+			for(var i=0; i<messages.length; i++) {
+				html += messages[i] + '<br />';
+			}
+			content.innerHTML = html;
+		} else {
+			console.log("There is a problem: ", data);
+		}
+	});
+
+	sendButton.onclick = function() {
+		var text = field.value;
+		socket.emit('send', { message: text });
+		field.value = "";
+	};
+
+	clearButton.onclick = function() {
+		content.innerHTML = "";
+		messages = [];
+	};
+}
